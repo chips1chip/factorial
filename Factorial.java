63a80cdbb7e5ba8c44ce42d7edcfa75ec29f6d56
Anjalinegi <anjalinegi26500@gmail.com>
2025-01-29 10:12:51 +0530

    Update Factorial.java

--- Factorial.java
+++ Factorial.java
@@ -2,14 +2,15 @@ import java.util.Scanner;
 
 public class Factorial {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        int fact=1;
-        for (int i= 1;i <=n;i++) {
-            fact=fact*i;
+        
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(); // Input number
+        int fact = 1; 
+        
+        for (int i = 1; i <= n; i++) {
+            fact *= i; // Calculate factorial
+            System.out.println(fact); // Output factorial
         }
-            System.out.println(fact);
-            sc.close();
-            
-        }
+        sc.close();
+    }
 }
